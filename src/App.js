ecb02fb1d096cef65bbe85f37deb3f11fa39abe9
James Nguyen <nguyentri1307@gmail.com>
2022-06-19 13:30:22 -0700

    create the sidebar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React,{useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent }from '@syncfusion/ej2-react-popups'
-
+import { Navbar, Footer, SideBar, ThemeSettings } from './components'
+import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, Line, ColorPicker, ColorMapping, Editor } from './pages'
 import './App.css'
 
 const App = ()=> {
-    const activeMenu = false
+    const activeMenu = true
     return(
         <div>
             <BrowserRouter >
@@ -20,11 +21,11 @@ const App = ()=> {
                     </div>
                     {activeMenu ? (
                         <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
-                            SideBar
+                            <SideBar />
                         </div>
                     ):(
                         <div className="w-0 dark:bg-secondary-dark-bg">
-                            SideBar
+                            <SideBar />
                         </div>
                     )}
                     <div className ={
@@ -33,34 +34,35 @@ const App = ()=> {
                         // :"dark:bg-main-bg bg-main-bg min-h-screen w full flex-2"
                         }>
                             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
-                                Navbar
+                                <Navbar />
                             </div>
                     </div>
                     <div>
                             <Routes>
                                 {/* dashboard */}
-                                <Route path ='/'element ="ECommerce" />
-                                <Route path ='/ecommerce'element ="ECommerce" />
+                                <Route path ='/'element ={<Ecommerce />} />
+                                <Route path ='/ecommerce'element ={<Ecommerce />} />
 
                                 {/* pages */}
-                                <Route path ='/orders'element ="Orders" />
-                                <Route path ='/employee'element ="Employee" />
-                                <Route path ='/customer'element ="Customers" />
+                                <Route path ='/orders'element ={<Orders />} />
+                                <Route path ='/employee'element ={<Employees />} />
+                                <Route path ='/customer'element ={<Customers />} />
 
                                 {/* APP */}
-                                <Route path ='/kanban'element ="Kanban" />
-                                <Route path ='/editor'element ="Editor" />
-                                <Route path ='/calendar'element ="Calendar" />
-                                <Route path ='/color-picker'element ="ColorPicker" />
+                                <Route path ='/kanban'element ={<Kanban />} />
+                                <Route path ='/editor'element ={<Editor />} />
+                                <Route path ='/calendar'element ={<Calendar />} />
+                                <Route path ='/color-picker'element ={<ColorPicker />} />
 
                                 {/* Charts */}
-                                <Route path ='/line'element ="Line" />
-                                <Route path ='/area'element ="Area" />
-                                <Route path ='/Pie'element ="Pie" />
-                                <Route path ='/financial'element ="Financial" />
-                                <Route path ='/color-mapping'element ="ColorMapping" />
-                                <Route path ='/pyramid'element ="Pyramid" />
-                                <Route path ='/stacked'element ="Stacked" />
+                                <Route path ='/line'element ={<Line />} />
+                                <Route path ='/area'element ={<Area />} />
+                                <Route path ='/Pie'element ={<Pie />} />
+                                <Route path ='/bar'element ={<Bar />} />
+                                <Route path ='/financial'element ={<Financial />} />
+                                <Route path ='/color-mapping'element ={<ColorMapping />} />
+                                <Route path ='/pyramid'element ={<Pyramid />} />
+                                <Route path ='/stacked'element ={<Stacked />} />
                             </Routes>
                         </div>
                 </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,60 @@
 import React from 'react'
+import {Link, NavLink }from 'react-router-dom';
+import { SiShopware } from 'react-icons/si';
+import { MdOutlineCancel } from 'react-icons/md';
+import {TooltipComponent} from '@syncfusion/ej2-react-popups'
 
-const Sidebar = () => {
+import { links } from '../data/dummy'
+
+
+const SideBar = () => {
+  const activeMenu = true;
+  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white  text-gray-70 text-md m-2'
+  const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md text-gray-700 text-gray-70 dark:hover:text-black  hover:bg-light-gray m-2'
+
   return (
-    <div>Sidebar</div>
+    <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
+      {activeMenu && (<>
+        <div className="flex justify-between items-center">
+          <Link to='/' onClick={()=> { }} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
+            <SiShopware /> <span>Shoppy</span>
+          </Link>
+          <TooltipComponent content="Menu" position='BottomCenter'>
+            <button type='button' 
+              onClick={()=>{}}
+              // maybe md:hidden 
+              className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block"
+              >
+              <MdOutlineCancel />
+            </button>
+          </TooltipComponent>
+        </div>
+        <div className='mt-10'>
+          {links.map((item) =>(
+            <div key={(item.title)}>
+              <p className="text-gray-400 m-3 mt-4 uppercase">
+              {item.title}
+              </p>
+              {item.links.map((link)=> (
+                <NavLink
+                  to={`/${link.name}`}
+                  key={link.name}
+                  onClick={()=> { }}
+                  className={({ isActive })=>
+                  isActive ? activeLink : normalLink  }
+                >
+                  {link.icon}
+                  <span className="capitalize">
+                    {link.name}
+                  </span>
+                </NavLink>
+              ))}
+            </div>
+          ))}
+        </div>
+      </>)}
+    </div>
   )
 }
 
-export default Sidebar
+export default SideBar
