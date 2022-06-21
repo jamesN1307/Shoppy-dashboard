7124f75cc8d6f8e84f19d5f6c6a25fd11aa931d7
James Nguyen <nguyentri1307@gmail.com>
2022-06-20 21:45:10 -0700

    finish

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,6 +1,6 @@
 export { default as Button } from './Button';
 export { default as ThemeSettings } from './ThemeSettings';
-export { default as SideBar } from './SideBar';
+export { default as Sidebar } from './Sidebar';
 // eslint-disable-next-line import/no-cycle
 export { default as Navbar } from './Navbar';
 export { default as Footer } from './Footer';
--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -1,36 +1,40 @@
-import React, {createContext, useContext, useState} from 'react';
+import React, { createContext, useContext, useState } from 'react';
 
 const StateContext = createContext();
 
-const initialState= {
-    chat: false,
-    cart: false,
-    userProfile: false, 
-    notifications: false,
-}
+const initialState = {
+  chat: false,
+  cart: false,
+  userProfile: false,
+  notification: false,
+};
 
-export const ContextProvider =({children}) => {
-    const [activeMenu, setActiveMenu] = useState(true);
-    
+export const ContextProvider = ({ children }) => {
+  const [screenSize, setScreenSize] = useState(undefined);
+  const [currentColor, setCurrentColor] = useState('#03C9D7');
+  const [currentMode, setCurrentMode] = useState('Light');
+  const [themeSettings, setThemeSettings] = useState(false);
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setIsClicked] = useState(initialState);
 
+  const setMode = (e) => {
+    setCurrentMode(e.target.value);
+    localStorage.setItem('themeMode', e.target.value);
+  };
 
+  const setColor = (color) => {
+    setCurrentColor(color);
+    localStorage.setItem('colorMode', color);
+  };
 
-    return (
-            // return stateContext.provider
+  const handleClick = (clicked) => setIsClicked({ ...initialState, [clicked]: true });
 
-        <StateContext.Provider 
-            value={{ 
-                activeMenu: activeMenu,
-                setActiveMenu: setActiveMenu
-                
-            }}
-        >
-            {children}
+  return (
+    // eslint-disable-next-line react/jsx-no-constructed-context-values
+    <StateContext.Provider value={{ currentColor, currentMode, activeMenu, screenSize, setScreenSize, handleClick, isClicked, initialState, setIsClicked, setActiveMenu, setCurrentColor, setCurrentMode, setMode, setColor, themeSettings, setThemeSettings }}>
+      {children}
+    </StateContext.Provider>
+  );
+};
 
-        </StateContext.Provider>
-    )
-}
-
-
-export const useStateContext =() => useContext(StateContext);
-    
+export const useStateContext = () => useContext(StateContext);
